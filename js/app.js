2bae554108714231fdf08888ef476cf719e25c2a
Travis <travishersh0@gmail.com>
2022-12-26 10:17:22 -0500

    Created a controls menu

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ const playGame = () => {
 ////////// MAIN MENU //////////
 
 const createMenu = () => {
+    menu.replaceChildren(null)
     // creating the title of the game
     const title = document.createElement('h1')
     title.innerText = 'Galaxy Invaders'
@@ -32,7 +33,7 @@ const createMenu = () => {
     controlsButton.innerText = 'Controls'
     controlsButton.style.webkitTextStroke = '2px #fff'
     controlsButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
-    // appending my creations to the menu
+    // appending buttons and title to the menu
     menu.appendChild(title)
     menu.appendChild(playButton)
     menu.appendChild(controlsButton)
@@ -41,10 +42,32 @@ const createMenu = () => {
     controlsButton.addEventListener('click', controls)
 }
 
-////////// CONTROLS AREA //////////
+////////// CONTROLS MENU //////////
 
 const controls = () => {
     menu.replaceChildren(null)
+    // Title
+    const controlsTitle = document.createElement('h1')
+    controlsTitle.innerText = 'Controls'
+    controlsTitle.style.webkitTextStroke = '2px #fff'
+    controlsTitle.style.fontSize = '75px'
+    // Controls
+    const controlsText = document.createElement('h3')
+    controlsText.innerText = 'You can move left and right with the A and D keys.\n You can also fire with spacebar.\n Try to survive as long as you can, and post your score to the leaderboard!'
+    controlsText.style.color = 'white'
+    controlsText.style.fontSize = '50px'
+    // Exit Button
+    const controlsExit = document.createElement('button')
+    controlsExit.innerText = 'Exit'
+    controlsExit.style.fontSize = '75px'
+    controlsExit.style.webkitTextStroke = '2px #fff'
+    controlsExit.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    // Appending
+    menu.appendChild(controlsTitle)
+    menu.appendChild(controlsText)
+    menu.appendChild(controlsExit)
+    // event listener
+    controlsExit.addEventListener('click', createMenu)
 }
 
 ////////// EVENT LISTENERS //////////
